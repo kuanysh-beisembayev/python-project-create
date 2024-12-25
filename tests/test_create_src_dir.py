65677449cc602e6ac83033145d5069efb3f87243
kuanysh-beisembayev <kuanysh.beisembayev@gmail.com>
2024-12-25 17:27:35 +0500

    test: remove conftest.py

--- tests/test_create_src_dir.py
+++ tests/test_create_src_dir.py
@@ -3,14 +3,14 @@ from pathlib import Path
 from create_python_project import create_src_dir
 
 
-def test(project_dir: Path) -> None:
-    create_src_dir(project_dir)
+def test(tmp_path: Path) -> None:
+    create_src_dir(tmp_path)
 
-    assert (project_dir / 'src').exists()
-    assert (project_dir / 'src').is_dir()
+    assert (tmp_path / 'src').exists()
+    assert (tmp_path / 'src').is_dir()
 
-    assert (project_dir / 'src/__init__.py').exists()
-    assert (project_dir / 'src/__init__.py').is_file()
+    assert (tmp_path / 'src/__init__.py').exists()
+    assert (tmp_path / 'src/__init__.py').is_file()
 
-    assert (project_dir / 'src/main.py').exists()
-    assert (project_dir / 'src/main.py').is_file()
+    assert (tmp_path / 'src/main.py').exists()
+    assert (tmp_path / 'src/main.py').is_file()
